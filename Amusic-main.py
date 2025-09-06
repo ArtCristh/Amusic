9e577c5eb1c4d96ae3ce905979add3b27e1c9ab1
ArtCristh <artcristh595@gmail.com>
2025-09-06 14:56:10 -0400

    Add files via upload
    funcionalidad de playlist terminada

--- Amusic-main.py
+++ Amusic-main.py
@@ -3,6 +3,7 @@ from variables_constantes import*
 import pygame
 import random
 from mutagen.mp3 import MP3
+import os
 
 
 pygame.mixer.init()
@@ -15,38 +16,48 @@ ventana = Tk()
 ventana.geometry("300x400")
 ventana.config(bg="black")
 ventana.resizable(0,0)
+#controlador de pausa binario
+binario = 0
 
 #funcion de prueba
-cancion_actual=""
-dire =""
+
+playlist =[]
+posicion = 0
 
 def abrir_musica():
-	global cancion_actual,posicion,x,dire,nombre
-	posicion = 0
-	x = 0
-	dire = filedialog.askopenfilenames(initialdir="/home",
-											title="Escoger cancion",
-										filetypes=(('mp3 files', '*.mp3*'),('All files', '*.*')))
-	x = len(dire)
-	cancion_actual=dire[posicion]
+	global playlist
+	
+	carpeta = filedialog.askdirectory(title= "busca canciones", )
 
-	nombre=cancion_actual.split("/")
-	nombre = cancion_actual[-1]
+	if carpeta :
+		canciones = []
+
+		for nombre_cancion in os.listdir(carpeta):
+			ruta_music = os.path.join(carpeta, nombre_cancion)
+			if os.path.isfile(ruta_music):
+				canciones.append(ruta_music)
+		
+	playlist = canciones
+
+	return playlist
+
+
+def pasar_music():
+	global posicion
+	posicion +=1
+	return posicion
 
 def play_music():
-	global cancion_actual,posicion
-	pygame.mixer.music.load(cancion_actual)
+	global playlist, posicion
+	pygame.mixer.music.load(playlist[posicion])
 	pygame.mixer.music.play()
-	posicion+=1
-def para_musica():
-	global cancion_actual,posicion
-	pygame.mixer.music.pause()
-def despausar():
-	global cancion_actual,posicion
-	pygame.mixer.music.unpause()
-def seguir():
-	global cancion_actual
-	pygame.mixer.music.play(-1)
+	pygame.mixer.music.play()
+
+
+
+
+
+	
 
 
 
@@ -54,21 +65,11 @@ def seguir():
 imagen_play=PhotoImage(file="play.png")
 imagen_pausa= PhotoImage(file="pause.png")
 imagen_cargar= PhotoImage(file="carga.png")
-imagen_unpause=PhotoImage(file="imagen_unpause.png")
-imagen= PhotoImage(file="monika.chr")
+
 
 boton_cargar=Button(ventana,image=imagen_cargar, width=56, height=56,highlightbackground ="black", padx=0,pady=1,bg="black",relief="flat",command=lambda:abrir_musica()).place(x=10,y=20)
 boton_play=Button(ventana,image= imagen_play,width=60, height=60,highlightbackground ="black", padx=0,pady=1,bg="black",relief="flat",command=lambda:play_music()).place(x=115,y=300)
-boton_pause=Button(ventana,image= imagen_pausa,width=60, height=60,highlightbackground ="black", padx=0,pady=1,bg="black",relief="flat",command=lambda:para_musica()).place(x=50,y=300)
-boton_despausar=Button(ventana,image= imagen_unpause,width=60, height=60,highlightbackground ="black", padx=0,pady=1,bg="black",relief="flat",command=lambda:despausar()).place(x=180,y=300)
-
-foto= Label(ventana,image=imagen).place(x=150,y=150)
-
-
-
-
-
-
+boton_pause=Button(ventana,image= imagen_pausa,width=60, height=60,highlightbackground ="black", padx=0,pady=1,bg="black",relief="flat",command=lambda:pasar_music()).place(x=50,y=300)
 
 
 
